92fb6b8c2d6e32344ca63afb419d976e31aff2f3
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2024-02-20 20:44:41 -0300

    Hook personalizado

--- src/components/CarrinhoSuspenso/ItemCarrinhoSuspenso/index.jsx
+++ src/components/CarrinhoSuspenso/ItemCarrinhoSuspenso/index.jsx
@@ -2,32 +2,12 @@ import React, { useContext } from "react";
 import Quantidade from "@/components/Quantidade";
 import Botao from "@/components/Botao";
 import ValorFormatado from "@/components/ValorFormatado";
-import { CarrinhoContext } from "@/context/CarrinhoContext";
+import { useCarrinhoContext } from "@/hooks/useCarrinhoContext";
 
 const ItemCarrinhoSuspenso = ({
   itemCarrinho,
-  removerProdutoCarrinho,
-  adicionarProduto,
 }) => {
-  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
-
-  function removerProduto(id) {
-    const produto = carrinho.find((itemDoCarrinho) => itemDoCarrinho.id === id);
-    const ehOUltimo = produto.quantidade === 1;
-
-    if (ehOUltimo) {
-      return setCarrinho((carrinhoAnterior) =>
-        carrinhoAnterior.filter((p) => p.id !== id)
-      )
-    }
-
-    setCarrinho((carrinhoAnterior) =>
-      carrinhoAnterior.map((p) => {
-        if (p.id === id) p.quantidade -= 1
-        return p;
-      })
-    );
-  }
+  const { removerProduto, adicionarProduto, removerProdutoCarrinho } = useCarrinhoContext();
 
   return (
     <li>
